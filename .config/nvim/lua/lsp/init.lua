66e6c97105d3b373511675c4bbfd5452485dfdd0
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-04-16 19:25:27 -0400

    Add border to hover window

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,28 +1,27 @@
 local lsp_path = vim.fn.stdpath("config") .. "/lua/lsp/"
 
 vim.diagnostic.config({
+  float = {
+    border = 'rounded'
+  },
   underline = {
     severity = { min = vim.diagnostic.severity.ERROR }
   },
   virtual_text = {
     current_line = true,
-    severity = { min = vim.diagnostic.severity.ERROR}
+    severity = { min = vim.diagnostic.severity.ERROR }
   },
   virtual_lines = false,
 })
 
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.hover, {desc = "[G]et [B]uffer"})
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {desc = "[G]et [D]efinitions"})
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {desc = "[G]et [R]eferences"})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {desc = "[C]ode [A]ctions"})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.hover, { desc = "[G]et [B]uffer" })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[G]et [D]efinitions" })
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "[G]et [R]eferences" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
 
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = 'single', max_height = 14, max_width = 100 }) end,
+  { desc = "Hover" })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, {
-    border = "single"
-  }
-)
 
 -- Auto formating on save
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -31,9 +30,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
     if not client:supports_method('textDocument/willSaveWaitUntil')
-      and client:supports_method('textDocument/formatting') then
+        and client:supports_method('textDocument/formatting') then
       vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = args.buf,
         callback = function()
           vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
@@ -49,4 +48,3 @@ for _, file in ipairs(vim.fn.readdir(lsp_path)) do
     require(module_name)
   end
 end
-
