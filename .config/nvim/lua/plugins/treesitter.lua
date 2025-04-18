15610c7faba684051b24c80583ff081db2db62b7
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-04-18 00:55:57 -0400

    Add treesitter defaults

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        ensure_installed = { "c", "lua", "go", "python", "vimdoc", "python", "markdown", "markdown_inline" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
