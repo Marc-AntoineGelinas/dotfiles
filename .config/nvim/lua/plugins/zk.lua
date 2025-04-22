fe55f855c8d7d3531531f8ffc8738e65d98ebad9
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-04-22 19:01:50 -0400

    Add zk-nvim

---
+++ .config/nvim/lua/plugins/zk.lua
@@ -0,0 +1,30 @@
+return {
+  "zk-org/zk-nvim",
+  config = function()
+    require("zk").setup({
+      picker = "snack_picker",
+      picker_options = {
+        snacks_picker = {
+          layout = {
+            preset = "telescope"
+          }
+        },
+      },
+    })
+
+    local opts = { noremap = true, silent = false }
+
+    vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew {title = vim.fn.input('Title: ') }<CR>", opts)
+
+    -- Open notes.
+    vim.keymap.set("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+    -- Open notes associated with the selected tags.
+    vim.keymap.set("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
+
+    -- Search for the notes matching a given query.
+    vim.keymap.set("n", "<leader>zf",
+      "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
+    -- Search for the notes matching the current visual selection.
+    vim.keymap.set("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
+  end
+}
