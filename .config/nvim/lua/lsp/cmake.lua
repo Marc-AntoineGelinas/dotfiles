15209752368ca53b3d842932c398da3cc7e08641
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-07-13 19:33:02 -0400

    Add configs for clang and cmake

---
+++ .config/nvim/lua/lsp/cmake.lua
@@ -0,0 +1,23 @@
+vim.lsp.config.cmake = {
+  cmd = { 'cmake-language-server' },
+  filetypes = { 'cmake' },
+  root_markers = {
+    'CMakePresets.json',
+    'CTestConfig.cmake',
+    '.git',
+    'build',
+    'cmake',
+  },
+  settings = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    },
+  },
+}
+
+vim.lsp.enable 'cmake'
