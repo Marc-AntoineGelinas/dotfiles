8f812a6f005383c7923dc171b2570e1f0ed7e4bb
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-08-18 22:22:34 -0400

    zknew now ask for title

--- .config/nvim/lua/plugins/zk.lua
+++ .config/nvim/lua/plugins/zk.lua
@@ -14,58 +14,35 @@ return {
 
         local opts = { noremap = true, silent = false }
         -- Open the link under the caret.
-        vim.keymap.set("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", { opts, desc = "Enter link under caret" })
-        vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew<CR>", { opts, desc = "Create new note" })
+        vim.keymap.set("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title =  vim.fn.input('Title: ') }<CR>", opts)
         -- Open notes.
-        vim.keymap.set(
-            "n",
-            "<leader>zo",
-            "<Cmd>ZkNotes { sort = { 'modified' } }<CR>",
-            { opts, desc = "List all notes" }
-        )
+        vim.keymap.set("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
         -- Open notes associated with the selected tags.
-        vim.keymap.set(
-            "n",
-            "<leader>zt",
-            "<Cmd>ZkTags<CR>",
-            { opts, desc = "List notes associated with selected tags" }
-        )
+        vim.keymap.set("n", "<leader>zt", "<Cmd>ZkTags<CR>", opts)
         -- Search for the notes matching a given query.
         vim.keymap.set(
             "n",
             "<leader>zf",
             "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
-            { opts, desc = "List all notes within a given query" }
+            opts
         )
         -- Create a new note in the same directory as the current buffer, using the current selection for title.
-        vim.keymap.set(
-            "v",
-            "<leader>znt",
-            ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>",
-            { opts, desc = "Create a new note with the current selected text as title" }
-        )
+        vim.keymap.set("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", opts)
         -- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
         vim.keymap.set(
             "v",
             "<leader>znc",
             ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
-            { opts, desc = "Createa a new note with selected text as content" }
+            opts
         )
+        vim.keymap.set("n", "<leader>zni", "<Cmd>ZkInsertLink<CR>", opts)
+        vim.keymap.set("v", "<leader>znl", ":'<,'>ZkInsertLinkAtSelection { dir = vim.fn.expand('%:p:h')}<CR>", opts)
         -- Open notes linking to the current buffer.
-        vim.keymap.set(
-            "n",
-            "<leader>zb",
-            "<Cmd>ZkBacklinks<CR>",
-            { opts, desc = "Open notes linked to the current note" }
-        )
+        vim.keymap.set("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", opts)
         -- Open notes linked by the current buffer.
-        vim.keymap.set("n", "<leader>zl", "<Cmd>ZkLinks<CR>", { opts, desc = "Open notes linked by the current note" })
+        vim.keymap.set("n", "<leader>zl", "<Cmd>ZkLinks<CR>", opts)
         -- Search for the notes matching the current visual selection.
-        vim.keymap.set(
-            "v",
-            "<leader>zf",
-            ":'<,'>ZkMatch<CR>",
-            { opts, desc = "Open notes matches selected visual text" }
-        )
+        vim.keymap.set("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
     end,
 }
